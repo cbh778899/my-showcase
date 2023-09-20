332df3b124e4774bf7bbad7d8ba5357d337e6e96
cbh778899 <cbh778899@outlook.com>
2023-09-20 13:09:32 +1000

    implement useLanguage hook

--- src/components/Declaimer.js
+++ src/components/Declaimer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/declaimer.css';
+import useLanguage from '../language';
 
 function Declaimer() {
+    const { languagePack } = useLanguage();
     return (
         <div className='declaimer'>
-            <span>2023 &copy; Lililele</span>
-            <span>Contact me at <a href="mailto:cbh778899@outlook.com">cbh778899@outlook.com</a></span>
+            <span>2023 &copy; {languagePack['Lililele']}</span>
+            <span>{languagePack['contact-me']}</span>
         </div>
     );
 }
--- src/components/nav-bar/TopNavBar.js
+++ src/components/nav-bar/TopNavBar.js
@@ -2,31 +2,33 @@ import React from 'react';
 import '../../styles/nav-bar/top_nav_bar.css';
 import NavBarItem from './NavBarItem';
 import SubMenu from './SubMenu';
-import { SUB_MENU_BLOCK, SUB_MENU_DROPDOWN } from '../../settings/types';
+import { LANGUAGE_CN, LANGUAGE_EN, SUB_MENU_BLOCK, SUB_MENU_DROPDOWN } from '../../settings/types';
 import SubMenuItem from './SubMenuItem';
 import { Link } from 'react-router-dom';
+import useLanguage from '../../language';
 
 function TopNavBar() {
+    const { languagePack, setLanguage } = useLanguage();
     return (
         <div className='top-nav-bar'>
             {/* <NavBarItem title='Home' /> */}
-            <Link to='/' className='nav-bar-item'>Home</Link>
-            <NavBarItem title='Functional Pages'>
+            <Link to='/' className='nav-bar-item'>{languagePack['Home']}</Link>
+            <NavBarItem title={languagePack['Functional Pages']}>
                 <SubMenu style={SUB_MENU_BLOCK}>
-                    <SubMenuItem title='Function 1' />
-                    <SubMenuItem title='Function 2' />
-                    <Link to='/' className='sub-menu-item'>This is a link</Link>
-                    <SubMenuItem title='Function 3' />
-                    <SubMenuItem title='Function 4' />
-                    <SubMenuItem title='Function 5' />
-                    <SubMenuItem title='Function 6' />
-                    <SubMenuItem title='Function 7' />
+                    <SubMenuItem title={languagePack['Function 1']} />
+                    <SubMenuItem title={languagePack['Function 2']} />
+                    <Link to='/' className='sub-menu-item'>{languagePack['This is a link']}</Link>
+                    <SubMenuItem title={languagePack['Function 3']} />
+                    <SubMenuItem title={languagePack['Function 4']} />
+                    <SubMenuItem title={languagePack['Function 5']} />
+                    <SubMenuItem title={languagePack['Function 6']} />
+                    <SubMenuItem title={languagePack['Function 7']} />
                 </SubMenu>
             </NavBarItem>
-            <NavBarItem title='Language'>
+            <NavBarItem title={languagePack['Language']}>
                 <SubMenu style={SUB_MENU_DROPDOWN}>
-                    <SubMenuItem title='English' />
-                    <SubMenuItem title='简体中文' />
+                    <SubMenuItem title='English' onClick={()=>{setLanguage(LANGUAGE_EN)}} />
+                    <SubMenuItem title='简体中文' onClick={()=>{setLanguage(LANGUAGE_CN)}} />
                 </SubMenu>
             </NavBarItem>
         </div>
