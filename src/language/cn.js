4a75d5b54db437c706dc2f1b3f194cb1b93b32c9
cbh778899 <cbh778899@outlook.com>
2023-09-21 13:55:52 +1000

    update translate for IDB related texts

--- src/language/cn.js
+++ src/language/cn.js
@@ -3,6 +3,10 @@ const cn = {
     'Home': '首页',
     'Functional Pages': '功能性界面',
     'Language': '语言',
+    // db
+    'IndexedDB Settings': 'IDB数据库设置',
+    'Initialize IndexedDB Manually': '手动初始化IDB数据库',
+    'Delete Current IndexedDB': '删除当前IDB数据库',
     // Declaimer & global
     'Lililele': '历历乐乐',
     'contact-me': <>如有需要，请联系<a href="mailto:cbh778899@outlook.com">cbh778899@outlook.com</a></>,
