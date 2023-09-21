bf00a990f92354b49764bdc51aaa7503896c5925
cbh778899 <cbh778899@outlook.com>
2023-09-21 19:36:42 +1000

    create empty pages for later to implement

---
+++ src/components/account/AccountDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/account/account_details.css';
+
+function AccountDetails() {
+    return (
+        <div className='account-details'>
+            
+        </div>
+    );
+}
+
+export default AccountDetails;
---
+++ src/components/account/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../styles/account/register.css';
+
+function Register({setLoginID, switchDisplayPage, languagePack}) {
+    return (
+        <div className='register'>
+        </div>
+    );
+}
+
+export default Register;
