910815cb0d8d6d5b72d3c52d336e9dceaf0ad483
cbh778899 <cbh778899@outlook.com>
2023-09-21 13:55:08 +1000

    add IDB types

--- src/settings/types.js
+++ src/settings/types.js
@@ -5,3 +5,10 @@ export const SUB_MENU_BLOCK = 'sub-menu-block';
 // language
 export const LANGUAGE_EN = 'en';
 export const LANGUAGE_CN = 'cn';
+
+// db
+export const IDB_NAME = 'lililele-showcase-indexed-db';
+export const IDB_ACCOUNT = 'account';
+
+export const IDB_MODE_READWRITE = 'readwrite';
+export const IDB_MODE_READONLY = 'readonly';
