1a65e61f43f337337fca5090f58a2a2af6e36461
cbh778899 <cbh778899@outlook.com>
2023-09-21 15:54:39 +1000

    add translation for db notifications

--- src/language/cn.js
+++ src/language/cn.js
@@ -1,3 +1,5 @@
+import { IDB_NAME } from "../settings/types";
+
 const cn = {
     // nav bar
     'Home': '首页',
@@ -6,7 +8,14 @@ const cn = {
     // db
     'IndexedDB Settings': 'IDB数据库设置',
     'Initialize IndexedDB Manually': '手动初始化IDB数据库',
-    'Delete Current IndexedDB': '删除当前IDB数据库',
+    'Delete Current IndexedDB': '删除IDB数据库',
+    // notification: db
+    'init-idb-success': '手动初始化IDB数据库成功!',
+    'del-idb-success': `删除IDB数据库${IDB_NAME}成功!`,
+    'del-idb-fail': `删除IDB数据库${IDB_NAME}失败!`,
+    'open-idb-timeout': '打开IDB数据库耗时过长(大于500毫秒),请尝试手动初始化IDB数据库!',
+    'clear-table-success': tb_name=>{return `清空IDB表${tb_name}成功!`},
+    'clear-table-fail': tb_name=>{return `清空IDB表${tb_name}失败!`},
     // Declaimer & global
     'Lililele': '历历乐乐',
     'contact-me': <>如有需要，请联系<a href="mailto:cbh778899@outlook.com">cbh778899@outlook.com</a></>,
--- src/language/en.js
+++ src/language/en.js
@@ -1,4 +1,13 @@
+import { IDB_NAME } from "../settings/types";
+
 const en = {
+    // notification: db
+    'init-idb-success': 'Manually Initialize IDB Success!',
+    'del-idb-success': `Delete DB ${IDB_NAME} success!`,
+    'del-idb-fail': `Delete DB ${IDB_NAME} Failed!`,
+    'open-idb-timeout': 'Open DB timeout (500ms), please consider try init IDB manually.',
+    'clear-table-success': tb_name=>{return `Clear IDB Schema ${tb_name} Success!`},
+    'clear-table-fail': tb_name=>{return `Clear IDB Schema ${tb_name} Failed!`},
     // Declaimer & global
     'contact-me': <>Please contact me at <a href="mailto:cbh778899@outlook.com">cbh778899@outlook.com</a> if needed.</>,
 }
