b0ab017908c5f8acf56cf2f574b7e52477ff45e3
cbh778899 <cbh778899@outlook.com>
2023-09-22 20:48:18 +1000

    implement basic structure of AccountDetails

--- src/components/account/AccountDetails.js
+++ src/components/account/AccountDetails.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/account/account_details.css';
+import { getByKeyPath } from '../../indexedDB';
+import { IDB_ACCOUNT } from '../../settings/types';
 
-function AccountDetails() {
+function AccountDetails({id, logout, languagePack}) {
+
+    const [userDetails, updateUserDetails] = useState();
+    
+    useEffect(()=>{
+        getByKeyPath(IDB_ACCOUNT, id, result=>{
+            console.log(result)
+            updateUserDetails(result);
+        }, null, ['password'])
+    // eslint-disable-next-line
+    }, [])
+
     return (
         <div className='account-details'>
-            
+            {
+                userDetails ? 
+                <>
+                <div>{languagePack['User ID']}: {userDetails.id}</div>
+                <div>{languagePack['Username']}: {userDetails.username}</div>
+                <div>{languagePack['Email']}: {userDetails.email}</div>
+                <div onClick={logout}>{languagePack['Logout']}</div>
+                </> : 
+                <div className='show-loading'>{languagePack['Loading...']}</div>
+            }
         </div>
     );
 }
