7cb03da06079fe84020bdb03b64d97b2af2a5869
cbh778899 <cbh778899@outlook.com>
2023-09-22 20:47:58 +1000

    add logout function

--- src/components/account/index.js
+++ src/components/account/index.js
@@ -4,15 +4,23 @@ import Login from './Login';
 import Register from './Register';
 import AccountDetails from './AccountDetails';
 import useLanguage from '../../language';
+import { toast } from 'react-toastify';
+import toastify_settings from '../../settings/toastify_settings';
 
 function Account() {
     const [loginID, setLoginID] = useState(null);
     const [displayPage, switchDisplayPage] = useState(ACCOUNT_LOGIN_PAGE)
     const { languagePack } = useLanguage();
 
+    function logout() {
+        setLoginID(null);
+        switchDisplayPage(ACCOUNT_LOGIN_PAGE);
+        toast.success(languagePack['Logout Success!'], toastify_settings);
+    }
+
     return (
         loginID !== null ?
-        <AccountDetails /> :
+        <AccountDetails {...{id: loginID, logout, languagePack}} /> :
         (displayPage === ACCOUNT_LOGIN_PAGE ?
         <Login {...{setLoginID, switchDisplayPage, languagePack}} /> :
         <Register {...{setLoginID, switchDisplayPage, languagePack}} />)
