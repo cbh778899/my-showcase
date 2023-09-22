c1007b8880c8e13cab6eba8b616dcc255e5a1333
cbh778899 <cbh778899@outlook.com>
2023-09-22 20:02:45 +1000

    add validation of password strength

--- src/actions/validators.js
+++ src/actions/validators.js
@@ -1,3 +1,20 @@
 export function isEmail(str) {
     return /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(str)
 }
+
+const password_testers = [
+    /\d+/g,
+    /[a-z]+/g,
+    /[A-Z]+/g,
+    /[,._!@#$%^&*()[\]\\/?+\-~`<>{}]+/g
+]
+
+export function passwordStrength(password) {
+    let strength = 0;
+    if(password.length < 8 || password.length > 20)
+        return 0;
+    password_testers.forEach(ptn=>{
+        if(password.match(ptn)) strength ++;
+    })
+    return strength;
+}
