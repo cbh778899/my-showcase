4f02ff74ddb6be617f7328c0e66daeabdcce659c
cbh778899 <cbh778899@outlook.com>
2023-09-22 20:03:50 +1000

    add registerAction

--- src/actions/account_actions.js
+++ src/actions/account_actions.js
@@ -1,14 +1,31 @@
-import { selectOneByColumn } from "../indexedDB";
+import { insert, selectOneByColumn } from "../indexedDB";
 import { IDB_ACCOUNT } from "../settings/types";
 import { isEmail } from "./validators";
 
 export function loginAction(account, password, callback) {
     selectOneByColumn(IDB_ACCOUNT, {
-        indexName: isEmail(account) ? 'username' : 'email',
+        indexName: isEmail(account) ? 'email' : 'username',
         indexValue: account,
         compareTo: { password }
     }, result => {
         if(result) callback(result.id);
         else callback(null)
+    })
+}
+
+export function registerAction(userDetails, callback) {
+    const { username, email, password } = userDetails;
+    insert(IDB_ACCOUNT, {
+        username, email, password
+    }, success => {
+        if(success){
+            selectOneByColumn(IDB_ACCOUNT, {
+                indexName: 'username',
+                indexValue: username
+            }, result => {
+                if(result) callback(result.id)
+                else callback(null)
+            })
+        } else callback(null)
     })
 }
