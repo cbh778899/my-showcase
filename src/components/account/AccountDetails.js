5b55b62a6c4ab091cf2c12a15bf88b47d3134f14
cbh778899 <cbh778899@outlook.com>
2023-09-23 10:54:47 +1000

    implement styled AccountDetails page

--- src/components/account/AccountDetails.js
+++ src/components/account/AccountDetails.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import '../../styles/account/account_details.css';
 import { getByKeyPath } from '../../indexedDB';
 import { IDB_ACCOUNT } from '../../settings/types';
+import { Person } from 'react-bootstrap-icons';
 
 function AccountDetails({id, logout, languagePack}) {
 
@@ -9,7 +10,6 @@ function AccountDetails({id, logout, languagePack}) {
     
     useEffect(()=>{
         getByKeyPath(IDB_ACCOUNT, id, result=>{
-            console.log(result)
             updateUserDetails(result);
         }, null, ['password'])
     // eslint-disable-next-line
@@ -20,10 +20,20 @@ function AccountDetails({id, logout, languagePack}) {
             {
                 userDetails ? 
                 <>
-                <div>{languagePack['User ID']}: {userDetails.id}</div>
-                <div>{languagePack['Username']}: {userDetails.username}</div>
-                <div>{languagePack['Email']}: {userDetails.email}</div>
-                <div onClick={logout}>{languagePack['Logout']}</div>
+                <div className='avatar'>{ userDetails.avatar ? <></> : <Person className='icon' /> }</div>
+                <div className='detail first no-edit'>
+                    <div className='detail-title'>{languagePack['User ID']}</div>
+                    {userDetails.id}
+                </div><div className='detail'>
+                    <div className='detail-title'>{languagePack['Username']}</div>
+                    {userDetails.username}
+                </div><div className='detail'>
+                    <div className='detail-title'>{languagePack['Email']}</div>
+                    {userDetails.email}
+                </div>
+                <div className='logout-container'>
+                    <div className='logout' onClick={logout}>{languagePack['Logout']}</div>
+                </div>
                 </> : 
                 <div className='show-loading'>{languagePack['Loading...']}</div>
             }
