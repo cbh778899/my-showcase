ba658090d7f318638b5f2aabbfd52ba3a079f10a
cbh778899 <cbh778899@outlook.com>
2023-09-23 20:29:41 +1000

    add edit submit & cancel edit button

--- src/components/account/EditableField.js
+++ src/components/account/EditableField.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { updateDetailsAction } from '../../actions/account_actions';
 import { toast } from 'react-toastify';
+import { CheckCircleFill, XCircleFill } from 'react-bootstrap-icons';
 
 function EditableField({fieldName, fieldValue, setFieldValue, origValue, requireUpdate, id, title, languagePack}) {
 
@@ -8,28 +9,42 @@ function EditableField({fieldName, fieldValue, setFieldValue, origValue, require
         setFieldValue(evt.target.value);
     }
 
-    function submitEdit(isCancel=false) {
-        if(!isCancel && fieldValue !== origValue && fieldValue) {
+    function submitEdit() {
+        if(fieldValue !== origValue && fieldValue) {
             const updateQuery = {}
             updateQuery[fieldName] = fieldValue;
             updateDetailsAction(id, updateQuery, result=>{
-                if(result) requireUpdate();
+                if(result) {
+                    toast.success(languagePack['update-success'](fieldName))
+                    requireUpdate();
+                }
                 else toast.error(languagePack['update-failed'](fieldName))
             })
         }
         setFieldValue(null);
     }
 
+    function cancelEdit() {
+        setFieldValue(null)
+    }
+
     return (
         fieldValue === null ?
-        <div className='detail' onClick={()=>setFieldValue(origValue)}>
+        <div className='detail' onClick={()=>setFieldValue(origValue)} 
+            title={languagePack['Click to edit']}>
             <div className='detail-title'>{languagePack[title]}</div>
             {origValue}
         </div> :
+        <>
+        <CheckCircleFill className='edit-icon finish-edit' onClick={submitEdit} />
+        <XCircleFill className='edit-icon cancel-edit' onClick={cancelEdit} />
         <input className='detail' placeholder={languagePack[`ask-input-${fieldName}`]} 
             name={fieldName} value={fieldValue} onInput={inputEvt}
             onKeyDown={evt=>evt.key === 'Enter' ? submitEdit() : undefined}
+            autoFocus 
         />
+        </>
+        
     );
 }
 
