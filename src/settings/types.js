8e7c8431bdc16a91af942a392458bd339f263fb6
cbh778899 <cbh778899@outlook.com>
2023-10-03 12:43:32 +1100

    add types for edit password

--- src/settings/types.js
+++ src/settings/types.js
@@ -6,6 +6,19 @@ export const SUB_MENU_BLOCK = 'sub-menu-block';
 export const ACCOUNT_LOGIN_PAGE = 0;
 export const ACCOUNT_REGISTER_PAGE = 1;
 
+// account edit password
+export const EDIT_PASSWORD_CHANNEL = 'lililele-showcase-account-edit-password';
+export const CHANNEL_STATUS_UNLOAD = 0;
+export const CHANNEL_STATUS_ACTIVE = 1;
+export const CHANNEL_STATUS_CLOSED = 2;
+export const EDIT_PASS_ID = 1;
+export const EDIT_PASS_EMAIL = 2;
+export const USER_EMAIL_VALIDATING = 'validating';
+export const USER_EMAIL_VALIDATED = true;
+export const USER_EMAIL_NOT_VALIDATED = false;
+export const REQUEST_CREDENTIAL = 'request-credential';
+export const SESSION_EXPIRED = 'session-expired';
+
 // language
 export const LANGUAGE_EN = 'en';
 export const LANGUAGE_CN = 'cn';
