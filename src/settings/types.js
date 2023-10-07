ff5a4ed407c059498a2e4a56731a97e9dd8022ea
cbh778899 <cbh778899@outlook.com>
2023-10-08 09:55:58 +1100

    update edit password types

--- src/settings/types.js
+++ src/settings/types.js
@@ -11,13 +11,18 @@ export const EDIT_PASSWORD_CHANNEL = 'lililele-showcase-account-edit-password';
 export const CHANNEL_STATUS_UNLOAD = 0;
 export const CHANNEL_STATUS_ACTIVE = 1;
 export const CHANNEL_STATUS_CLOSED = 2;
-export const EDIT_PASS_ID = 1;
-export const EDIT_PASS_EMAIL = 2;
 export const USER_EMAIL_VALIDATING = 'validating';
-export const USER_EMAIL_VALIDATED = true;
-export const USER_EMAIL_NOT_VALIDATED = false;
+export const USER_EMAIL_VALIDATED = 'validated';
+    // msg.type
+export const EDIT_PASS_ID = 'edit-pass-id';
+export const EDIT_PASS_EMAIL = 'edit-pass-email';
+export const REQUEST_PAIR = 'request-pair';
+export const ANSWER_PAIR = 'answer-pair';
 export const REQUEST_CREDENTIAL = 'request-credential';
+export const ANSWER_CREDENTIAL = 'answer-credential';
 export const SESSION_EXPIRED = 'session-expired';
+export const PASSWORD_CHANGED = 'password-changed';
+export const CHANNEL_ONLINE = 'channel-online';
 
 // language
 export const LANGUAGE_EN = 'en';
@@ -29,3 +34,6 @@ export const IDB_ACCOUNT = 'account';
 
 export const IDB_MODE_READWRITE = 'readwrite';
 export const IDB_MODE_READONLY = 'readonly';
+
+// special
+export const DIGITS_AND_LETTERS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
