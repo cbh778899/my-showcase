b6b13e56a4ba26a07163db94bae69d2d2dd66ee4
cbh778899 <cbh778899@outlook.com>
2023-10-08 09:56:10 +1100

    add generateID

--- src/actions/generator.js
+++ src/actions/generator.js
@@ -1,3 +1,11 @@
+import { DIGITS_AND_LETTERS } from "../settings/types";
+
 export function generateVerificationCode() {
     return [...Array(4)].map(()=>Math.floor(Math.random() * 10)).join``
+}
+
+export function generateID() {
+    return [...Array(10)].map(()=>{
+        return DIGITS_AND_LETTERS[Math.floor(Math.random() * DIGITS_AND_LETTERS.length)]
+    }).join``;
 }
