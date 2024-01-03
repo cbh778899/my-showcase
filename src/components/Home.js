33d9f0631281fa9123cfb5a0b07956cac636f87e
cbh778899 <cbh778899@outlook.com>
2024-01-03 11:21:01 +1100

    implement home page design

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,19 @@ import '../styles/home.css';
 function Home() {
     return (
         <div className='home'>
-            
+            <div className='home-contents'>
+                <section>
+                    <div className='section-text-box'>
+                        <div className='title'>Home Page</div>
+                        <div className='content'>
+                            Welcome to my homepage!<br/>
+                            There are plenty of interesting functions implemented only using frontend techniques waiting for explore.
+                            <br/><br/>
+                            Scroll down to see more descriptions!
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
