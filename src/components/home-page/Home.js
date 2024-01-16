05179088a056fb9ccd85579e9d1dfdce303935c4
cbh778899 <cbh778899@outlook.com>
2024-01-16 20:35:41 +1100

    determine if should redirect when loaded

--- src/components/home-page/Home.js
+++ src/components/home-page/Home.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../styles/home-page/home.css';
 import useLanguage from '../../language';
 import HomeSection from './HomeSection';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getSearchParams } from '../../actions/getter';
 
 function Home() {
 
     const { languagePack } = useLanguage();
+
+    const navigate = useNavigate();
+    const { search } = useLocation();
+
+    useEffect(() => {
+        const queryParams = getSearchParams(search);
+        
+        if(queryParams['edit-password']) {
+            navigate('/account/edit-password')
+        }
+    // eslint-disable-next-line
+    }, [search])
 
     return (
         <div className='home'>
