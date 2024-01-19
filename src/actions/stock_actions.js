492e526b19d23f609c639f428505097c6de31df1
cbh778899 <cbh778899@outlook.com>
2024-01-19 15:21:38 +1100

    implement getCurrentStock and addItem

--- src/actions/stock_actions.js
+++ src/actions/stock_actions.js
@@ -1,9 +1,25 @@
+import { getAll, insert } from "../indexedDB";
+import { IDB_STOCK_ITEM } from "../settings/types";
+
 export function getCurrentStock(callback) {
-    callback([]);
+    getAll(IDB_STOCK_ITEM, callback)
 }
 
-export function addItem({itemName, itemPrice, itemStock}, operatorID, callback) {
-    
+export function addItem(itemName, itemPrice, itemStock, operatorID, callback, price = null) {
+    insert(IDB_STOCK_ITEM, {
+        itemName, itemPrice, itemStock
+    }, result => {
+        if(result) {
+            addStockHistory(
+                result,
+                Date.now(),
+                itemStock,
+                price === null ? -itemStock*itemPrice : price,
+                operatorID
+            );
+            callback(true);
+        } else callback(false)
+    })
 }
 
 export function editStock(itemID, stockChanged, operatorID, callback, totalPrice = null) {
@@ -14,6 +30,6 @@ export function rmItem(itemID, callback) {
 
 }
 
-function addStockHistory({itemID, time, stockChange, price, operatorID}) {
+function addStockHistory(itemID, time, stockChange, price, operatorID) {
 
 }
