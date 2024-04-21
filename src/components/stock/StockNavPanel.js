6b13b357d62d2296d5e456a816243f8a7a4c9651
cbh778899 <cbh778899@outlook.com>
2024-04-21 12:09:25 +1000

    impl navpanel

--- src/components/stock/StockNavPanel.js
+++ src/components/stock/StockNavPanel.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import PopupWindow from '../popup'
+import usePopup from '../../popup'
 import '../../styles/stock/stock_nav_panel.css';
+import ManageOperatorPage from './ManageOperatorPage';
 
-function StockNavPanel() {
+function StockNavPanel({languagePack}) {
+
+    const manageOperatorController = usePopup(true);
+    const saleController = usePopup(true);
+
     return (
         <div className='stock-nav-panel'>
-            
+            <div 
+                className='menu-item clickable' 
+                onClick={manageOperatorController.showModal}
+            >{ languagePack['Manage Operators'] }</div>
+            <div 
+                className='menu-item clickable' 
+                onClick={saleController.showModal}
+            >{ languagePack['Sale'] }</div>
+
+            <ManageOperatorPage controller={manageOperatorController} languagePack={languagePack} />
+            <PopupWindow controller={saleController}>
+                sale controller
+            </PopupWindow>
         </div>
     );
 }
