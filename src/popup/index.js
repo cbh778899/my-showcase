803d36ee43f4b8213f65cadfa6c8886f07310476
cbh778899 <cbh778899@outlook.com>
2024-04-23 13:42:51 +1000

    can run function on open or close popup window

--- src/popup/index.js
+++ src/popup/index.js
@@ -2,9 +2,13 @@ import { useEffect, useRef, useState } from "react"
 
 const useDialog = typeof HTMLDialogElement === 'function';
 
-function usePopup(closeOnClick = false) {
+function usePopup(closeOnClick = false, autoRun = {onOpen: [], onClose: []}) {
+
+    const { onOpen, onClose } = autoRun;
 
     const [on, switchStatus] = useState('disabled');
+    const [onPopupOpen, setOnPopupOpen] = useState(onOpen)
+    const [onPopupClose, setOnPopupClose] = useState(onClose)
     const windowRef = useRef();
 
     useEffect(() => {
@@ -23,17 +27,30 @@ function usePopup(closeOnClick = false) {
 
     function show() {
         switchStatus('enabled')
+        runAutoRun('open')
     }
 
     function showModal() {
         switchStatus('modal')
+        runAutoRun('open')
     }
 
     function close() {
         switchStatus('disabled')
+        runAutoRun('close')
     }
 
-    return { show, showModal, close, useDialog, on, closeOnClick, windowRef }
+    function addAutoRun(timing, func) {
+        if(timing === 'open') setOnPopupOpen([...onPopupOpen, func])
+        else if(timing === 'close') setOnPopupClose([...onPopupClose, func]);
+    }
+
+    function runAutoRun(timing) {
+        if(timing === 'open') onPopupOpen.forEach(func=>func());
+        if(timing === 'close') onPopupClose.forEach(func=>func());
+    }
+
+    return { show, showModal, close, addAutoRun, useDialog, on, closeOnClick, windowRef }
 }
 
 export default usePopup;
