928594b6c2cb59c4f2134da768d164b1c56d3fd4
cbh778899 <cbh778899@outlook.com>
2024-04-23 10:17:37 +1000

    rewrite indexedDB CURD, change to promise

--- src/indexedDB/index.js
+++ src/indexedDB/index.js
@@ -10,238 +10,262 @@ export function updateLanguagePack(langpk) {
 }
 
 export function initDB(manual = false) {
-    // find latest version
-    let latestVersion = Math.max(...db_settings.idbVersions.map(e=>e.version));
-    const db_request = window.indexedDB.open(db_settings.name, latestVersion);
+    return new Promise(resolve => {
+        // find latest version
+        let latestVersion = Math.max(...db_settings.idbVersions.map(e=>e.version));
+        const db_request = window.indexedDB.open(db_settings.name, latestVersion);
 
-    db_request.onsuccess = function() {
-        db = this.result;
-        if(manual)
-            toast.info(languagePack['init-idb-success']);
-    }
+        db_request.onsuccess = function() {
+            db = this.result;
+            manual && toast.info(languagePack['init-idb-success']);
+            resolve(true);
+        }
 
-    db_request.onerror = function() {
-        db = null;
-        toast.error(this.error);
-    }
+        db_request.onerror = function() {
+            db = null;
+            toast.error(this.error);
+            resolve(null);
+        }
 
-    db_request.onupgradeneeded = function (evt) {
-        const currDB = evt.currentTarget.result;
-        db_settings.idbVersions.forEach(idbVersion => {
-            if(evt.oldVersion < idbVersion.version) {
-                idbVersion.objectStores.forEach(objStoreAttr => {
-                    if(objStoreAttr.command === IDB_CMD_NEW) {
-                        const objStore = currDB.createObjectStore(objStoreAttr.name, objStoreAttr.structureSettings);
-                        objStoreAttr.schemas.forEach(schema=>{
-                            objStore.createIndex(schema.name, schema.name, schema.settings || {});
-                        })
-                    } else if(objStoreAttr.command === IDB_CMD_UPDATE) {
-                        const store = db_request.transaction.objectStore(objStoreAttr.name);
-                        objStoreAttr.newSchemas && objStoreAttr.newSchemas.forEach(schema=>{
-                            store.createIndex(schema.name, schema.name, schema.settings || {});
-                        })
-                        objStoreAttr.rmSchemas && objStoreAttr.rmSchemas.forEach(schema=>{
-                            store.deleteIndex(schema);
-                        })
-                    }
-                })
-            }
-        })
-    }
+        db_request.onupgradeneeded = function (evt) {
+            const currDB = evt.currentTarget.result;
+            db_settings.idbVersions.forEach(idbVersion => {
+                if(evt.oldVersion < idbVersion.version) {
+                    idbVersion.objectStores.forEach(objStoreAttr => {
+                        if(objStoreAttr.command === IDB_CMD_NEW) {
+                            const objStore = currDB.createObjectStore(objStoreAttr.name, objStoreAttr.structureSettings);
+                            objStoreAttr.schemas.forEach(schema=>{
+                                objStore.createIndex(schema.name, schema.name, schema.settings || {});
+                            })
+                        } else if(objStoreAttr.command === IDB_CMD_UPDATE) {
+                            const store = db_request.transaction.objectStore(objStoreAttr.name);
+                            objStoreAttr.rmSchemas && objStoreAttr.rmSchemas.forEach(schema=>{
+                                store.deleteIndex(schema);
+                            })
+                            objStoreAttr.newSchemas && objStoreAttr.newSchemas.forEach(schema=>{
+                                store.createIndex(schema.name, schema.name, schema.settings || {});
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    })
 }
 
-async function getObjStore(name, mode) {
-    if(!db) {
-        let count = 0;
-        // wait for db to be ready, max 500ms
-        while(!db && count < 5) {
-            await new Promise(s=>setTimeout(s, 100));
-            count ++;
-        }
-        if(!db) throw new Error(
-            languagePack['open-idb-timeout']
-        );
+function getObjStore(name, mode) {
+    try {
+        return db.transaction(name, mode).objectStore(name);
+    } catch(error) {
+        toast.error(error);
     }
-    return db.transaction(name, mode).objectStore(name);
 }
 
 // CRUD
 
-export async function insert(storeName, data, callback) {
-    try {
-        const req = (await getObjStore(storeName, IDB_MODE_READWRITE)).add(data);
-        req.onsuccess = () => callback(req.result);
-        req.onerror = () => callback(false);
-    } catch (error) {
-        toast.error(error.message);
-        callback(false);
-    }
+export function insert(storeName, data) {
+    return new Promise(resolve => {
+        try {
+            const req = getObjStore(storeName, IDB_MODE_READWRITE).add(data);
+            req.onsuccess = () => resolve(req.result);
+            req.onerror = () => resolve(null);
+        } catch (error) {
+            toast.error(error.message);
+            resolve(null);
+        }
+    })
 }
 
-export async function selectOneByColumn(storeName, query, callback) {
-    try {
-        const store = await getObjStore(storeName, IDB_MODE_READONLY);
-        const req = store.index(query.indexName);
-        req.get(query.indexValue).onsuccess = function() {
-            const queryResult = this.result;
-            if(!queryResult) {
-                callback(false);
-                return;
-            }
-            if(query.compareTo) {
-                for(const key in query.compareTo) {
-                    if(queryResult[key] !== query.compareTo[key]) {
-                        callback(false);
-                        return;
+export function getOne(storeName, get_query, exclude = null) {
+    return new Promise(async resolve => {
+        try {
+            const store = getObjStore(storeName, IDB_MODE_READONLY);
+            if(store.keyPath in get_query) {
+                resolve(await getByID(storeName, get_query[store.keyPath], get_query, exclude, store))
+            } else {
+                const get_query_key_pairs = 
+                Object.keys(get_query)
+                .map(key=>{return {key, value: get_query[key]}})
+                // get by the first provided key in get_query
+                const req = store.index(get_query_key_pairs[0].key);
+                req.openCursor().onsuccess = evt => {
+                    const cursor = evt.target.result;
+                    if(cursor) {
+                        const result = cursor.value;
+                        for(const { key, v } of get_query_key_pairs) {
+                            if(v !== result[key]) {
+                                // if not match, go to next record
+                                cursor.continue();
+                                return;
+                            }
+                        }
+                        // all matched, do else
+                        exclude && exclude.forEach(e=>delete result[e]);
+                        resolve(result);
+                    } else {
+                        // all results not match
+                        resolve(null);
                     }
                 }
+
+                req.onerror = error => {
+                    toast.error(error);
+                    resolve(null);
+                }
             }
-            query.exclude && query.exclude.forEach(e=>delete queryResult[e]);
-
-            callback(queryResult);
+        } catch(error) {
+            toast.error(error);
+            resolve(null)
         }
-
-        req.onerror = () => callback(false);
-    } catch (error) {
-        toast.error(error.message);
-    }
+    })
 }
 
-export async function getAll(storeName, callback, query = null, store = null) {
-    try {
-        store = store || await getObjStore(storeName, IDB_MODE_READONLY);
-        const req = store.getAll()
+export function getAll(storeName, validate = null, exclude = null, store = null) {
+    return new Promise(resolve => {
+        try {
+            store = store || getObjStore(storeName, IDB_MODE_READONLY);
+            const req = store.getAll();
+            
+            req.onsuccess = () => {
+                let results = req.result;
+                if(validate) {
+                    results = results.filter(entry => {
+                        let matched = true;
+                        for(const key in validate) {
+                            if(entry[key] !== validate[key]) {
+                                matched = false;
+                                break;
+                            }
+                        }
+                        return matched;
+                    })
+                }
+                if(exclude) {
+                    for(let i = 0; i < results.length; i++) {
+                        exclude.forEach(e=>delete results[i][e]);
+                    }
+                }
+
+                resolve(results)
+            }
+
+            req.onerror = () => resolve([])
+        } catch (error) {
+            toast.error(error);
+            resolve(null);
+        }
+    })
+}
+
+export function getByID(storeName, id, validate = null, exclude = null, store = null) {
+    return new Promise(resolve => {
+        try {
+            store = store || getObjStore(storeName, IDB_MODE_READONLY);
+            const req = store.get(id);
         
-        req.onsuccess = () => {
-            let result = req.result;
-            if(query && query.compareTo) {
-                result = result.filter(entry => {
-                    let matched = true;
-                    for(const key in query.compareTo) {
-                        if(entry[key] !== query.compareTo[key]) {
-                            matched = false;
-                            break;
+            req.onsuccess = function() {
+                const result = this.result;
+                if(result && validate) {
+                    for(const key in validate) {
+                        if(result[key] !== validate[key]) {
+                            resolve(null)
+                            return;
                         }
                     }
-                    return matched;
-                })
+                }
+                result && exclude && exclude.forEach(e=>delete result[e]);
+                resolve(result);
             }
-            if(query && query.exclude && query.exclude.length) {
-                result = result.map(res => {
-                    query.exclude.forEach(e=>delete res[e]);
-                    return res;
-                })
-            }
-            
-            callback(result)
+            req.onerror = () => resolve(null);
+        } catch (error) {
+            toast.error(error.message);
+            resolve(null);
         }
-
-        req.onerror = () => callback([])
-    } catch (error) {
-        toast.error(error);
-        callback(false);
-    }
+    })
 }
 
-export async function getByKeyPath(storeName, key, callback, extra = {store: null, exclude: null, compareTo: null}) {
-    try {
-        let { store, exclude, compareTo } = extra
-        store = store || await getObjStore(storeName, IDB_MODE_READONLY);
-        const req = store.get(key);
-    
-        req.onsuccess = function() {
-            const result = this.result;
-            if(result && compareTo) {
-                for(const index in compareTo) {
-                    if(result[index] !== compareTo[index]) {
-                        callback(false)
-                        return;
+export function update(storeName, record_with_id, update_query) {
+    return new Promise(resolve => {
+        try {
+            const store = getObjStore(storeName, IDB_MODE_READWRITE);
+            const req = store.put({...record_with_id, ...update_query});
+            req.onsuccess = () => resolve(true);
+            req.onerror = () => resolve(null);
+        } catch(error) {
+            toast.error(error.message);
+            resolve(null);
+        }
+    })
+}
+
+export function deleteByID(storeName, id) {
+    return new Promise(resolve => {
+        try {
+            const store = getObjStore(storeName, IDB_MODE_READWRITE);
+            const req = store.delete(id);
+            req.onsuccess = () => resolve(true);
+            req.onerror = () => resolve(null);
+        } catch (error) {
+            toast.error(error.message);
+            resolve(null);
+        }
+    })
+}
+
+export function deleteAllByColumn(storeName, delete_query = null) {
+    return new Promise(async resolve => {
+        try {
+            if(!delete_query) resolve(await clearAll(storeName));
+            else {
+                const store = getObjStore(storeName, IDB_MODE_READWRITE);
+                const key = store.keyPath;
+                const results = await getAll(storeName, delete_query, null, store);
+                
+                let delete_count = results.length;
+                results.forEach(result =>{
+                    const req = store.delete(result[key]);
+                    req.onsuccess = () => {
+                        if(-- delete_count === 0) {
+                            resolve(true);
+                        }
                     }
-                }
-            }
-            result && exclude && exclude.forEach(e=>delete result[e]);
-            callback(result);
-        }
-        req.onerror = () => callback(false);
-    } catch (error) {
-        toast.error(error.message);
-        callback(false);
-    }
-}
-
-export async function update(storeName, query, callback) {
-    try {
-        const store = await getObjStore(storeName, IDB_MODE_READWRITE);
-        getByKeyPath(storeName, query.id, result=>{
-            if(result) {
-                for(const key in query.updateQuery) {
-                    result[key] = query.updateQuery[key];
-                }
-                const req = store.put(result);
-
-                req.onsuccess = () => callback(true);
-                req.onerror = () => callback(false);
-            }
-        }, {store})
-    } catch(error) {
-        toast.error(error.message);
-        callback(false);
-    }
-}
-
-export async function deleteByKeyPath(storeName, key, callback) {
-    try {
-        const store = await getObjStore(storeName, IDB_MODE_READWRITE);
-        getByKeyPath(storeName, key, exist => {
-            if(exist) {
-                const req = store.delete(key);
-                req.onsuccess = () => callback(true);
-                req.onerror = () => callback(false);
-            } else callback(false);
-        }, {store});
-    } catch (error) {
-        toast.error(error.message);
-        callback(false);
-    }
-}
-
-export async function deleteAllByColumn(storeName, query, callback) {
-    try {
-        const store = await getObjStore(storeName, IDB_MODE_READWRITE);
-        getAll(storeName, results => {
-            if(results) {
-                results.forEach(e=>{
-                    store.delete(e.key);
+                    req.onerror = error => {
+                        toast.error(error);
+                        resolve(null);
+                    }
                 })
-                callback(true);
-            } else callback(false);
-        }, query, store)
-    } catch (error) {
-        toast.error(error.message);
-        callback(false);
-    }
+            }
+        } catch (error) {
+            toast.error(error.message);
+            resolve(null);
+        }
+    })
 }
 
-export async function clearAll(storeName, callback) {
-    try {
-        const req = (await getObjStore(storeName, IDB_MODE_READWRITE)).clear();
-        req.onsuccess = () => {
-            toast.info(languagePack['clear-table-success'](storeName));
-            callback(true);
+export function clearAll(storeName) {
+    return new Promise(resolve => {
+        try {
+            const req = getObjStore(storeName, IDB_MODE_READWRITE).clear();
+            req.onsuccess = () => {
+                toast.info(languagePack['clear-table-success'](storeName));
+                resolve(true);
+            }
+            req.onerror = () => {
+                toast.info(languagePack['clear-table-fail'](storeName));
+                resolve(null);
+            }
+        } catch (error) {
+            toast.error(error.message);
+            resolve(null);
         }
-        req.onerror = () => {
-            toast.info(languagePack['clear-table-fail'](storeName));
-            callback(false);
-        }
-    } catch (error) {
-        toast.error(error.message);
-        callback(false);
-    }
+    })
 }
 
 export function deleteDB() {
-    if(db) db.close()
-    const req = window.indexedDB.deleteDatabase(IDB_NAME);
-    req.onsuccess = () => {toast.info(languagePack['del-idb-success'])}
-    req.onerror = () => toast.error(languagePack['del-idb-fail'])
+    return new Promise(resolve => {
+        if(db) db.close()
+        const req = window.indexedDB.deleteDatabase(IDB_NAME);
+        req.onsuccess = () => {toast.info(languagePack['del-idb-success']); resolve(true)}
+        req.onerror = () => {toast.error(languagePack['del-idb-fail']); resolve(null)}
+    })
 }
